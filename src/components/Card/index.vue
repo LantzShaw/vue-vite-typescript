537967bc6b3039a12ac43d0f441b41718cb123d5
Lantz <lantzshaw@163.com>
2022-07-05 16:21:13 +0800

    initialize project

---
+++ src/components/Card/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Card Component</div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+  name: 'Card',
+})
+</script>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Home Page</div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+  name: 'Home',
+})
+</script>
